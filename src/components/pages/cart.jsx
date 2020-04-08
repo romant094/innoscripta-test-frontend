82c3e6ef5e48ae14cd38a4eb1373574dbf6492bf
Anton Romankov <ar@askque.ru>
2020-04-08 18:52:09 +0300

    Big instance of logo

--- src/components/pages/cart.jsx
+++ src/components/pages/cart.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import {useSelector} from 'react-redux';
 
 export const Cart = () => {
+    const cart = useSelector(state => state.cart);
+    // TODO replace to utils
+    const total = cart.reduce((acc, item) => acc + (item.price * item.count), 0);
+
     return (
         <div>
-            Cart
+            {
+                cart.map(item => (
+                    <div className='mb-5' key={item.title}>
+                        <div>{item.title}</div>
+                        <div>{item.count}</div>
+                        <div>{item.price}</div>
+                    </div>
+                ))
+            }
+            Total: {total}
         </div>
     );
 };
