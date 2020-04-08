862fd56058535fadd931e40ab0875e4941ab3955
Anton Romankov <ar@askque.ru>
2020-04-08 18:51:03 +0300

    Reducer behavior

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,50 @@
-const initialState = {};
+const initialState = {
+    cart: []
+};
 
 export const reducer = (state = initialState, action) => {
-    // const {type, payload} = action;
+    const {cart} = state;
+    const {type, payload} = action;
 
-    // switch (type) {
-    //     default:
-    // }
-    return state;
+    switch (type) {
+        case 'UPDATE_CART':
+            return {
+                ...state,
+                cart: payload
+            };
+        case 'ADD_ITEM_TO_CART':
+
+            return {
+                ...state,
+                cart: [...cart, payload]
+            };
+        case 'DELETE_ITEM_FROM_CART':
+            return {
+                ...state,
+                cart: [
+                    ...cart.slice(0, payload),
+                    ...cart.slice(payload)
+                ]
+            };
+        case 'CHANGE_CART_ITEM_COUNT':
+            const {id, count} = payload;
+            const editableItem = cart[id];
+            editableItem.count = editableItem.count + count;
+
+            return {
+                ...state,
+                cart: [
+                    ...cart.slice(0, id),
+                    editableItem,
+                    ...cart.slice(id)
+                ]
+            };
+        case 'CLEAR_CART':
+            return {
+                ...state,
+                cart: []
+            };
+        default:
+            return state;
+    }
 };
