42bef31e0c3e232d8e3ae0684f1018cd81943342
Anton Romankov <ar@askque.ru>
2020-04-08 14:34:05 +0300

    Centralized promo

--- src/components/constants.js
+++ src/components/constants.js
@@ -4,6 +4,9 @@ import salad from '../assests/images/nav-links/salad.jpg';
 import drink from '../assests/images/nav-links/drink.jpg';
 import sushi from '../assests/images/nav-links/sushi.jpg';
 import wok from '../assests/images/nav-links/wok.jpg';
+import promo1 from '../assests/images/promo/promo1.jpeg';
+import promo2 from '../assests/images/promo/promo2.jpeg';
+import promo3 from '../assests/images/promo/promo3.jpeg';
 
 export const NAV_LINKS = [
     {title: 'Pizza', url: '/pizza', imgUrl: pizza},
@@ -12,4 +15,19 @@ export const NAV_LINKS = [
     {title: 'Sushi', url: '/sushi', imgUrl: sushi},
     {title: 'Wok', url: '/wok', imgUrl: wok},
     {title: 'Drinks', url: '/drinks', imgUrl: drink}
+];
+
+export const PROMO_ITEMS = [
+    {
+        src: promo1,
+        altText: 'Promo 1'
+    },
+    {
+        src: promo2,
+        altText: 'Promo 2'
+    },
+    {
+        src: promo3,
+        altText: 'Promo 3'
+    }
 ];
--- src/components/pages/home/home.jsx
+++ src/components/pages/home/home.jsx
@@ -1,43 +1,20 @@
 import React from 'react';
 import styled from 'styled-components';
 import {PromoCarousel} from '../../promo-carousel';
-import promo1 from '../../../assests/images/promo1.jpeg';
-import promo2 from '../../../assests/images/promo2.jpeg';
-import promo3 from '../../../assests/images/promo3.jpeg';
 import {HomeLinks} from './home-links';
+import {PROMO_ITEMS} from '../../constants';
 
-const PromoWrapper = styled.div`
+const Wrapper = styled.div`
   margin-bottom: 60px;
 `;
 
-const HomeLinksWrapper = styled(PromoWrapper)`
-
-`;
-
-export const Home = () => {
-    const promoItems = [
-        {
-            src: promo1,
-            altText: 'Promo 1'
-        },
-        {
-            src: promo2,
-            altText: 'Promo 2'
-        },
-        {
-            src: promo3,
-            altText: 'Promo 3'
-        }
-    ];
-
-    return (
-        <div>
-            <PromoWrapper>
-                <PromoCarousel items={promoItems} settings={{interval: 5000}} />
-            </PromoWrapper>
-            <HomeLinksWrapper>
-                <HomeLinks />
-            </HomeLinksWrapper>
-        </div>
-    );
-};
+export const Home = () => (
+    <React.Fragment>
+        <Wrapper>
+            <PromoCarousel items={PROMO_ITEMS} settings={{interval: 5000}} />
+        </Wrapper>
+        <Wrapper>
+            <HomeLinks />
+        </Wrapper>
+    </React.Fragment>
+);
