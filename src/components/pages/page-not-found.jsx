011c28b9133d6fb74ee9146df41ae60b65d8a155
Anton Romankov <ar@askque.ru>
2020-04-19 17:55:35 +0300

    404 page

--- src/components/pages/page-not-found.jsx
+++ src/components/pages/page-not-found.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
+import {Container} from 'reactstrap';
+import styled from 'styled-components';
+import {Link} from 'react-router-dom';
+
+const Text = styled.div`
+    margin-bottom: .3rem;
+    font-weight: 300;
+`;
+
+const ComponentWrapper = styled.div`
+  color: rgb(115,121,140);
+  text-align: center;
+  font-size: 20px;
+`;
 
 export const PageNotFound = () => {
     return (
-        <div>
-            Sorry, noting here
-        </div>
+        <Container>
+            <ComponentWrapper>
+                <Text>Sorry, there is noting here...</Text>
+                <Text>Return to the <Link to='/'>Home page</Link>.</Text>
+            </ComponentWrapper>
+        </Container>
     );
 };
