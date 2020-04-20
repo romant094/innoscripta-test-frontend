59d57711c1f6cf53243ea70e5af1332eb4c5fa01
юзер <uzer@MacBook-Pro-uzer.local>
2020-04-20 10:12:12 +0300

    Hot fix

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,2 @@
-export const HEROKU_API_URI = 'https://aqueous-tor-69859.herokuapp.com/api/v1';
+export const HEROKU_API_URI = '/api/v1';
 export const LOCAL_API_URI = 'http://localhost:9000/api/v1';
